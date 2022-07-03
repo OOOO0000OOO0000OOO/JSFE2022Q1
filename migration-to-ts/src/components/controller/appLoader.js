f4f3cfdf4cd052141bb8179329000034d985933d
k0ntinuum <st088265@student.spbu.ru>
2022-07-03 17:27:45 +0300

    feat: install TS; set webpack and eslint; get API key

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '27b9fa9542854eb2a6eec1a3c5733f66', // получите свой ключ https://newsapi.org/
         });
     }
 }
--- migration-to-ts/webpack.dev.config.js
+++ migration-to-ts/webpack.dev.config.js
@@ -4,6 +4,10 @@ module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: path.resolve(__dirname, '../dist'),
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        open: true,
+        hot: true,
     },
 };

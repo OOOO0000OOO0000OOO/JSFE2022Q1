7d108e310ca2b35dbfb81c38e3330d89eeb925b5
k0ntinuum <st088265@student.spbu.ru>
2022-07-04 10:50:43 +0300

    feat: pass type to generic class in appLoader.ts

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -1,6 +1,6 @@
 import Loader from './loader';
 
-class AppLoader extends Loader {
+class AppLoader extends Loader<string> {
     constructor() {
         super('https://newsapi.org/v2/', {
             apiKey: '27b9fa9542854eb2a6eec1a3c5733f66', // получите свой ключ https://newsapi.org/

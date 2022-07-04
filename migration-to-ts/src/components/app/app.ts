69a3a8ae2a912bd068250438cd81453fad078978
k0ntinuum <st088265@student.spbu.ru>
2022-07-04 09:17:35 +0300

    feat: add type annotations in app.ts, appView.ts

--- migration-to-ts/src/components/app/app.ts
+++ migration-to-ts/src/components/app/app.ts
@@ -1,17 +1,21 @@
 import AppController from '../controller/controller';
-import { AppView } from '../view/appView';
+import AppView from '../view/appView';
+import { IData } from './IData';
 
 class App {
+    controller: AppController;
+    view: AppView;
+
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
     start() {
-        document
-            .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-        this.controller.getSources((data) => this.view.drawSources(data));
+        (<HTMLElement>document.querySelector('.sources')).addEventListener('click', (e: MouseEvent) =>
+            this.controller.getNews(e, (data: IData) => this.view.drawNews(data))
+        );
+        this.controller.getSources((data: IData): void => this.view.drawSources(data));
     }
 }
 

d9f6ab4e456c5ccbb2f6223b58cbfd85ff15a78e
k0ntinuum <st088265@student.spbu.ru>
2022-07-18 15:40:39 +0300

    init: start onlive-store task; add assets; adjust TS, webpack, eslint

---
+++ online-store/webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingWebpackPlugin = require('eslint-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const baseConfig = {
+  entry: path.resolve(__dirname, './src/index'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.ts$/i,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './dist/online-store'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        '**/*',
+        '!.git',
+      ],
+    }),
+    new EslingWebpackPlugin({ extensions: 'ts' }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "./src/assets/", to: "./public/" },
+      ],
+    }),
+  ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === "prod";
+  const envConfig = isProductionMode
+    ? {
+      mode: "production",
+    }
+    : {
+      mode: "development",
+      devtool: "inline-source-map",
+      devServer: {
+        static: {
+          directory: path.resolve(__dirname, "/dist/online-store"),
+        },
+        open: true,
+        hot: true,
+      },
+    };
+  return merge(baseConfig, envConfig);
+};

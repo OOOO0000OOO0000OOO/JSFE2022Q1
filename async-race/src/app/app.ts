7339945a07e6d6bac4cac2d306d27c57ee79dc25
k0ntinuum <st088265@student.spbu.ru>
2022-08-11 16:03:11 +0300

    feat: add html components

--- async-race/src/app/app.ts
+++ async-race/src/app/app.ts
@@ -1,22 +1,35 @@
+import header from './header.html';
+import footer from './footer.html';
 import Control from '../common/control';
 import GarageController from '../garage/garageController';
 import GarageView from '../garage/garageView';
 import Router from '../router/router';
 import GarageAdapter from '../services/garageAdapter';
+
 import './global.css';
+import './header.css';
+import './footer.css';
 
 class AppController extends Control {
+  private garageController: GarageController;
+
+  private router: Router;
+
   constructor(parentNode: HTMLElement) {
     super({ parentNode, className: 'application' });
+    parentNode.prepend(new Control({ parentNode: null, className: 'header', content: header }).node);
 
     const garageAdapter = new GarageAdapter();
     const garageView = new GarageView();
-    const garageController = new GarageController(garageAdapter, garageView);
+    this.garageController = new GarageController(garageAdapter, garageView);
 
-    new Router(this.node, [
+    this.router = new Router(this.node, [
       ['/garage', garageView.node],
       ['/winners', new Control({ parentNode: null, content: 'winners' }).node],
-    ]).init('/garage');
+    ]);
+    this.router.init('/garage');
+
+    parentNode.append(new Control({ parentNode, className: 'footer', content: footer }).node);
   }
 }
 
